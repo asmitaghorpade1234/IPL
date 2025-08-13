cfc071658c45a45dd20145038c19335ad826e10c
Admin <Admin@DESKTOP-UTRG7HJ>
2025-08-13 17:31:24 +0530

    initial commit

--- src/repository/TeamRepository.java
+++ src/repository/TeamRepository.java
@@ -6,7 +6,9 @@ public class TeamRepository {
 	
 	//should return MI team details
 	public static Team getMITeamDetails(){
+		
 		Team team = new Team();
+		
 		team.setId(101);
 		team.setTeamName("Mumbai Indians");
 		team.setCaptionName("Hardik Pandya");
@@ -15,4 +17,121 @@ public class TeamRepository {
 		team.setnRR(2.3f);
 		return team;		
 	}
+	
+	public static Team getCSKTeamDetails(){
+		
+		Team team1 = new Team();
+		
+		team1.setId(102);
+		team1.setTeamName("Chennai Superking");
+		team1.setCaptionName("M.S.Dhoni");
+		team1.setCoachName("Jaywardhan");
+		team1.setQualified(false);
+		team1.setnRR(3.4f);
+		return team1;		
+	}		
+	
+	public static Team getRRTeamDetails() {
+		
+		Team team2 = new Team();
+		
+		team2.setId(103);
+		team2.setTeamName("Rajastahan Royal");
+		team2.setCaptionName("Sanju Samson");
+		team2.setCoachName("Rahul Dravid");
+		team2.setQualified(false);
+		team2.setnRR(5.1f);
+		return team2;
+		}
+		
+		public static Team getKKRTeamDetails() {
+			
+			Team team3 = new Team();
+			
+			team3.setId(104);
+			team3.setTeamName("Kolkata Knight Rider");
+			team3.setCaptionName("Ajinkya Rahane");
+			team3.setCoachName("Chandu Borde");
+			team3.setQualified(false);
+			team3.setnRR(1.2f);
+			return team3;
+		}
+		
+		public static Team getLSGTeamDetails() {
+			
+			Team team4 = new Team();
+			
+			team4.setId(105);
+			team4.setTeamName("Lucknow Super Giants");
+			team4.setCaptionName("RISHABH PANT");
+			team4.setCoachName("JUSTIN LANGER");
+			team4.setQualified(true);
+			team4.setnRR(1.2f);
+			return team4;
+		}		
+
+		public static Team getRCBTeamDetails() {
+			
+			Team team5 = new Team();
+			
+			team5.setId(106);
+			team5.setTeamName("Royal Chanllenger Bengluru");
+			team5.setCaptionName("Rajat Patidar");
+			team5.setCoachName("Andy Flower");
+			team5.setQualified(true);
+			team5.setnRR(1.2f);
+			return team5;
+		}
+	
+		public static Team getDCTeamDetails() {
+	
+		Team team6 = new Team();
+	
+		team6.setId(107);
+		team6.setTeamName("Delhi Capital");
+		team6.setCaptionName("Axar Patel");
+		team6.setCoachName("Hemang Badani");
+		team6.setQualified(false);
+		team6.setnRR(1.2f);
+		return team6;
+	   }
+		
+		public static Team getPKTeamDetails() {
+			
+			Team team7 = new Team();
+			
+			team7.setId(108);
+			team7.setTeamName("Punjab Kings XI");
+			team7.setCaptionName("Shreyas ayyar");
+			team7.setCoachName("Ricky Ponting");
+			team7.setQualified(true);
+			team7.setnRR(3.4f);
+			return team7;
+		}
+		
+		public static Team getGTTeamDetails() {
+			
+			Team team8 = new Team();
+			
+			team8.setId(109);
+			team8.setTeamName("Gujrat Titans");
+			team8.setCaptionName("Shubhman Gill");
+			team8.setCoachName("Ashish Nehra");
+			team8.setQualified(false);
+			team8.setnRR(1.2f);
+			return team8;
+		}
+		
+		public static Team getSRHTeamDetails() {
+			
+			Team team9 = new Team();
+			
+			team9.setId(110);
+			team9.setTeamName("SunRisers Hyderbad");
+			team9.setCaptionName("Pat Cummins");
+			team9.setCoachName("Daniel Vettori");
+			team9.setQualified(false);
+			team9.setnRR(3.4f);
+			return team9;
+			}	
 }
--- src/service/TeamService.java
+++ src/service/TeamService.java
@@ -5,6 +5,7 @@ import repository.TeamRepository;
 
 public class TeamService {
 	public void printTeamDetails() {
+		
 		Team team = TeamRepository.getMITeamDetails();
 		System.out.println(team.getId());
 		System.out.println(team.getTeamName());
@@ -12,6 +13,96 @@ public class TeamService {
 		System.out.println(team.getCoachName());
 		System.out.println(team.getnRR());
 		System.out.println(team.isQualified());
+		System.out.println("------------------------------------------------");
+	
+		Team team1 = TeamRepository.getCSKTeamDetails();
+		System.out.println(team1.getId());
+		System.out.println(team1.getTeamName());
+		System.out.println(team1.getCaptionName());
+		System.out.println(team1.getCoachName());
+		System.out.println(team1.getnRR());
+		System.out.println(team1.isQualified());
+		System.out.println("------------------------------------------------");
+		
+		Team team2 = TeamRepository.getCSKTeamDetails();
+		System.out.println(team2.getId());
+		System.out.println(team2.getTeamName());
+		System.out.println(team2.getCaptionName());
+		System.out.println(team2.getCoachName());
+		System.out.println(team2.getnRR());
+		System.out.println(team2.isQualified());
+		System.out.println("------------------------------------------------");
+		
+		Team team3 = TeamRepository.getRRTeamDetails();
+		System.out.println(team3.getId());
+		System.out.println(team3.getTeamName());
+		System.out.println(team3.getCaptionName());
+		System.out.println(team3.getCoachName());
+		System.out.println(team3.getnRR());
+		System.out.println(team3.isQualified());
+		System.out.println("------------------------------------------------");
+		
+		Team team4 = TeamRepository.getKKRTeamDetails();
+		System.out.println(team4.getId());
+		System.out.println(team4.getTeamName());
+		System.out.println(team4.getCaptionName());
+		System.out.println(team4.getCoachName());
+		System.out.println(team4.getnRR());
+		System.out.println(team4.isQualified());
+		System.out.println("------------------------------------------------");
+		
+		Team team5 = TeamRepository.getLSGTeamDetails();
+		System.out.println(team5.getId());
+		System.out.println(team5.getTeamName());
+		System.out.println(team5.getCaptionName());
+		System.out.println(team5.getCoachName());
+		System.out.println(team5.getnRR());
+		System.out.println(team5.isQualified());
+		System.out.println("------------------------------------------------");
+		
+		Team team6 = TeamRepository.getRCBTeamDetails();
+		System.out.println(team6.getId());
+		System.out.println(team6.getTeamName());
+		System.out.println(team6.getCaptionName());
+		System.out.println(team6.getCoachName());
+		System.out.println(team6.getnRR());
+		System.out.println(team6.isQualified());
+		System.out.println("------------------------------------------------");
+		
+		Team team7 = TeamRepository.getDCTeamDetails();
+		System.out.println(team7.getId());
+		System.out.println(team7.getTeamName());
+		System.out.println(team7.getCaptionName());
+		System.out.println(team7.getCoachName());
+		System.out.println(team7.getnRR());
+		System.out.println(team7.isQualified());
+		System.out.println("------------------------------------------------");
+		
+		Team team8 = TeamRepository.getPKTeamDetails();
+		System.out.println(team8.getId());
+		System.out.println(team8.getTeamName());
+		System.out.println(team8.getCaptionName());
+		System.out.println(team8.getCoachName());
+		System.out.println(team8.getnRR());
+		System.out.println(team8.isQualified());
+		System.out.println("------------------------------------------------");
+		
+		Team team9 = TeamRepository.getGTTeamDetails();
+		System.out.println(team9.getId());
+		System.out.println(team9.getTeamName());
+		System.out.println(team9.getCaptionName());
+		System.out.println(team9.getCoachName());
+		System.out.println(team9.getnRR());
+		System.out.println(team9.isQualified());
+		System.out.println("------------------------------------------------");
+		
+		Team team10 = TeamRepository.getSRHTeamDetails();
+		System.out.println(team10.getId());
+		System.out.println(team10.getTeamName());
+		System.out.println(team10.getCaptionName());
+		System.out.println(team10.getCoachName());
+		System.out.println(team10.getnRR());
+		System.out.println(team10.isQualified());
 	}
 
 }
